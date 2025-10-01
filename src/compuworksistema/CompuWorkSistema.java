5953df54fec28e4602640446301c3b92ee2a40c4
vanesahincapie-ai <vanesa.hincapie@est.iudigital.edu.co>
2025-10-01 18:53:07 -0500

    Subo todo el proyecto CompuWork con código fuente

---
+++ src/compuworksistema/CompuWorkSistema.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compuworksistema;
+
+/**
+ *
+ * @author luis.cardona
+ */
+//Vanesa Hincapié Martínez 
+//Marisol Guarin Escobar
+import CompuWorkViews.VentanaPrincipal;
+import java.util.Date;
+
+public class CompuWorkSistema {
+    public static void main(String[] args) {
+       // try {
+         // Departamento depTI = new Departamento("Tecnología");
+
+           // Empleado e1 = new EmpleadoPermanente(
+                 //   0,
+                   // "Juan",
+                   // "Pérez",
+                   // "Analista",
+                   // 2500000,
+                   // new Date(),
+                   // "Seguro médico, Vacaciones",
+                   // new Date()
+           // );
+
+           // Empleado e2 = new EmpleadoTemporal(
+                  //  0,
+               //     "Ana",
+                  //  "Gómez",
+                  //  "Asistente",
+                   // 1800000,
+                  //  new Date(),
+                   // "Seguro médico básico",
+                   // true
+          //  );
+
+      
+            //depTI.agregarEmpleado(e1);
+           // depTI.agregarEmpleado(e2);
+
+           
+           // depTI.agregarEmpleado(e1);
+
+        
+           // System.out.println("Lista de empleados en " + depTI.getNombre() + " ===");
+           // System.out.println(depTI.mostrarEmpleados());
+
+            
+           // System.out.println("\n=== Reportes individuales ===");
+          //  System.out.println(e1.generarReporteDesempeño());
+           // System.out.println(e2.generarReporteDesempeño());
+
+        
+            //System.out.println("\n=== Reporte del departamento ===");
+           // for (Empleado e : depTI.getEmpleados()) {
+            //    System.out.println(e.generarReporteDesempeño());
+           // }
+
+            
+           // depTI.eliminarEmpleado(e1);
+           // System.out.println("\nDespués de eliminar a Juan:");
+            //System.out.println(depTI.mostrarEmpleados());
+
+        // } catch (Exception e) {
+          //  System.out.println("Error: " + e.getMessage());
+       // }
+        
+       VentanaPrincipal  ventana= new VentanaPrincipal();
+       ventana.setVisible(true);
+    }
+}
